1ad2f0fccbd3d9749db08372f80f64614aa88ad3
rasmus.gillberg <xxyaymanyayxx@hotmail.com>
2020-10-06 13:40:32 +0200

    Saker hände

--- src/Cell.java
+++ src/Cell.java
@@ -1,25 +1,44 @@
+import sun.font.TrueTypeFont;
+
 public class Cell {
 
     private String color;
     private boolean Rules;
+    private boolean[] cells;
 
-    public String getLife(boolean Rules) {
+    }
+    public boolean getLife(boolean cells) {
 
         if (Rules = false) {
-            color = "white";
-        }
-        else {
-            color = "black";
-        }
-        return color;
-    }
+            for (int i = 0; i < 500; i++) {
+                cells[i] = false;
+            }
+        } else {
+            for (int i = 0; i < 500; i++) {
+                cells[i] = true;
+            }
 
-    public Cell(String color) {
-        this.color = color;
+
+            /*
+            Kolla på kod
+
+            int[] grid = new int[36];
+            for (int i=0; i < 36; i++){
+                grid([i]=(Math.random());
+        }
+             */
+
+        return cells;
+    }
+    public String getColor() {
+        return color;
     }
 
     public boolean isRules() {
         return Rules;
     }
-    
+
+    public boolean[] getCells() {
+        return cells;
+    }
 }
--- src/Grid.java
+++ src/Grid.java
@@ -1,5 +1,18 @@
-public class Grid {
+public class Grid extends Cell {
 
+    private boolean[] cells;
 
+    @Override
+    public String getLife(boolean Rules) {
+        return super.getLife(Rules);
+    }
 
+    public Grid(String color, boolean[] cells) {
+        super(color);
+        this.cells = cells;
+    }
+
+    public Grid(boolean[] cells) {
+        this.cells = cells;
+    }
 }
